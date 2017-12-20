8345a177f1ddbc8c82128d8454b0702b50776f09
walliam <liuweiwalliam@163.com>
2017-12-20 15:19:58 +0800

    upd

--- home.js
+++ home.js
@@ -80,6 +80,7 @@ var content = [
     ['airBnb 规范', 'https://www.w3cschool.cn/rtuhtw/3idpuozt.html'],
     ['airBnb JSX规范', 'https://github.com/JasonBoy/javascript/tree/master/react'],
     ['可视化正则', 'https://jex.im/regulex/'],
+    ['babel-在线实验', 'http://babeljs.io'],
     ['redux 中文文档', 'http://www.redux.org.cn/docs/introduction/Motivation.html'],
     ['swagger-local', 'http://hrmapi.dev.mila66.com/'],
     ['swagger-test', 'http://hrmapi.x.mila66.com'],
